bb7332aa200f4e878475248cb0c3a85c9f77bea7
wagthedog2021@naver.com <wagthdog2021@naver.com>
2022-03-21 19:51:23 +0900

    81185

--- Four Project.c
+++ Four Project.c
@@ -140,4 +140,141 @@ int desk_top = 30;
 // C++ = 스네이크
 
 
+
+#include <stdio.h>
+
+int main()
+{
+
+	//증감 연산자
+		/*
+		int number_1 = 10;
+		int number_2 = 10;
+
+		// ++ 변수 앞에 오게 되면 먼저 연산을 수행한 후에 값을 증가시킵니다.
+		printf("전위 증가 연산자 : %d\n", ++number_1);
+
+		// --가 변수 앞에 오게되면 먼저 연산을 수행한 후에 값을 감소시킵니다.
+		printf("전위 감소 연산자 : %d\n", --number_2);
+
+		//변수 뒤에 ++가 오게되면 나중에 값을 증가시키고 연산을 수행합니다.
+		printf("후기 증가 연산자 : %d\n", number_1++);
+
+		//변수 뒤에 --가 오게되면 나중에 값을 감소시키고 연산을 수행합니다.
+		printf("후기 감소 연산자 : %d\n", number_2--);
+
+		printf("number_1의 값 : %d\n", number_1);
+		printf("number_2의 값 : %d\n", number_2);
+
+
+		/*
+		int resul_1 = ++number
+		int resul_2 = --number
+
+
+		*/
+
+		// while 문
+
+		/*
+		// 변수를 이용하여 값을 전달해서 조건이 불이치되도록 설정하고 while문을 탈출하는 과제
+		int person = 3;
+
+		while (person > 2) // "1 : 조건이 참이다" 라는 의미입니다.
+		{
+			printf("공장의 인원을 재 배치해주세요.");
+			scanf_s("%d", & person); //변수 안에 있는 메모리 넘겨줘야 , 깜박 대기 < 숫자 입력
+
+
+			if (person > 2)
+			{
+				printf("공장이 돌아가고 있습니다.\n");
+			}
+
+		}
+		*/
+
+		//do while 문
+		/*
+		do
+		{
+			printf("조건이 틀렸지만, 실행됩니다.");
+
+		}
+		while (0 > 2); // 3>2 조건이 맞으면 계속 실행됨.
+
+		return 0;
+		*/
+
+		// for 문
+		/*
+		for (int i = 0; i <= 5; i++) //5번 실행됨 <= 6번 실행됨
+		{
+			printf("안녕하세요? 집에 가고 싶습니다.\n");
+
+
+		}
+		*/
+
+		/*
+		// 1~10 까지의 합을 구해주세요.
+		int value = 0;
+
+		for (int i = 1; i <= 10; i++ )
+		{
+			value += i;
+		 }
+
+		printf("1~10 까지의 합 : %d", value); // 55 나옴
+
+
+
+		*/
+
+		/*
+		int value = 0;
+
+		for (int i = 5; i >= 1; i--)
+		{
+			printf("%d\n", i);
+		}
+
+		//5
+		//4
+		//3
+		//2
+		//1
+		*/
+
+		/*
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					printf("j의 값 : %d\n", j); // j - 1 하면 -숫자 됨.
+				}
+				printf("\n");
+			}
+			*/
+
+
+			// 구구단
+
+	for (int i = 1; i < 10; i++)
+	{
+		for (int j = 1; j < 10; j++)
+		{
+			printf("%d * %d = %d\n", i, j, i * j);
+		}
+		printf("\n");
+
+
+	}
+
+
+
+
+	return 0;
+
+}
 }
